aa7675875d20964ba713811ff2d73adf9406e052
nwdavis <nathanieldavis89@gmail.com>
2017-08-23 19:00:19 -0400

    adding friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,68 @@
-var friends = {
-    "name":"Nathaniel",
-    "photo":"",
-    "scores":[
-        4,
-        2,
-        3,
-        4,
-        4,
-        2,
-        1,
-        2,
-        5,
-        3
-    ]
-};
+var friends = [
+    {
+        "name":"Nathaniel",
+        "photo":"",
+        "scores":[
+            4,
+            2,
+            3,
+            4,
+            4,
+            2,
+            1,
+            2,
+            5,
+            3
+        ]
+    },
+    {
+        "name": "Bertram",
+        "photo": "",
+        "scores":[
+            3,
+            4,
+            1,
+            1,
+            2,
+            5,
+            2,
+            4,
+            5,
+            5
+        ]
+    },
+    {
+        "name": "Jackie",
+        "photo": "",
+        "scores":[
+            1,
+            2,
+            2,
+            4,
+            5,
+            1,
+            3,
+            4,
+            5,
+            2
+        ]
+    },
+    {
+        "name": "John",
+        "photo": "",
+        "scores":[
+            2,
+            4,
+            5,
+            4,
+            2,
+            1,
+            1,
+            3,
+            4,
+            3
+        ]
+    }   
+];
 
 module.exports = friends;
