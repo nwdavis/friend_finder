bd75eab71d58545b6283592aafd0b529a0f6f791
nwdavis <nathanieldavis89@gmail.com>
2017-08-25 23:44:36 -0400

    making changes

--- app/data/checker.js
+++ app/data/checker.js
@@ -1,32 +1,32 @@
 var friends = require('./friends.js');
 
 module.exports = function(user){
-    console.log("checking");
 
     var userScores = user.scores;
-    var chosenFriend = '';
+    var chosenFriend = {};
     //temp variable for comparing difference in scores
     var k;
 
+//runs through all the friends in friends.js and compares the incoming user data
     friends.forEach(function(friend){
         
         var fScores = friend.scores;
         var uScores = userScores;
         var totalDiff;
-        
+              
 
         for (i=0; i < uScores.length; i++){
 
             var j = difference(parseInt(uScores[i]), parseInt(fScores[i]));
-            console.log(j)
             totalDiff += j;
-
+            
         }
 
         function difference(a, b) { return Math.abs(a - b); }
 
         if (totalDiff < k){
             console.log("pushing to k")
+            k = totalDiff;
             chosenFriend = friend.name;
         }
 
